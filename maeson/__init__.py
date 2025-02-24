a4a40c08c17e892095d9c73bd98cf64dcfec227a
dzfinch <dryver2206@gmail.com>
2025-02-24 18:44:56 -0500

    modified:   maeson/__init__.py

--- maeson/__init__.py
+++ maeson/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Dryver Finch"""
 __email__ = "dryver2206@gmail.com"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 # Import the function from its module
 from .gps_metrics import calculate_accuracy_metrics

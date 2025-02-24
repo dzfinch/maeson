2c026b04febebbf2a92835b21e4d5850f50a62c0
dzfinch <dryver2206@gmail.com>
2025-02-24 18:20:13 -0500

    modified:   maeson/__init__.py 	modified:   notebooks/example_usage.ipynb

--- maeson/__init__.py
+++ maeson/__init__.py
@@ -3,3 +3,9 @@
 __author__ = """Dryver Finch"""
 __email__ = "dryver2206@gmail.com"
 __version__ = "0.0.1"
+
+# Import the function from its module
+from .gps_metrics import calculate_accuracy_metrics
+
+# Optionally, define __all__ to control what gets imported with `from maeson import *`
+__all__ = ["calculate_accuracy_metrics"]
